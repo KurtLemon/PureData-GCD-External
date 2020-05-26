555eaae0bc1e546207bbf327a50cc0a170ccc166
KurtLemon <klamon@zagmail.gonzaga.edu>
2020-05-25 17:18:31 -0700

    Added handling of a reset and list message as input.

--- gcd.c
+++ gcd.c
@@ -15,18 +15,12 @@ typedef struct _gcd {
 } t_gcd;
 
 void gcd_setVals(t_gcd *x, t_floatarg f1, t_floatarg f2) {
-    x -> val_A = (f1 <= 0) ? 1 : f1;
-    x -> val_B = (f2 <= 0) ? 1 : f2;
+    x->val_A = (f1 <= 0) ? 1 : f1;
+    x->val_B = (f2 <= 0) ? 1 : f2;
 }
 
-/**
- * Assign and store the incoming float value. Triggered when a float is received.
- *
- * @param x
- */
-void gcd_onFloat(t_gcd *x) {
-    post("[gcd] Float value received");
-    // TODO: Receive and save float value
+void gcd_resetVal(t_gcd *x) {
+    x->gcd_val = 0;
 }
 
 /**
@@ -35,7 +29,22 @@ void gcd_onFloat(t_gcd *x) {
  * @param x
  */
 void gcd_onBang(t_gcd *x) {
-    outlet_float(x -> out_gcd, x -> gcd_val);
+    outlet_float(x->out_gcd, x->gcd_val);
+}
+
+void gcd_onReset(t_gcd *x) {
+    gcd_resetVal(x);
+}
+
+void gcd_onList(t_gcd *x, t_symbol *s, t_int argc, t_atom *argv) {
+    switch(argc) {
+        case 2:
+            gcd_setVals(x, atom_getfloat(argv), atom_getfloat(argv + 1));
+            gcd_resetVal(x);
+            break;
+        default:
+            error("[gcd]: received incorrect number of arguments to calculate new GCD");
+    }
 }
 
 void gcd_onSet_A(t_gcd *x, t_floatarg f) {
@@ -49,16 +58,16 @@ void gcd_onSet_B(t_gcd *x, t_floatarg f) {
 void *gcd_new(t_floatarg f1, t_floatarg f2) {
     t_gcd *x = (t_gcd *) pd_new(gcd_class);
     gcd_setVals(x, f1, f2);
-    x -> out_gcd = outlet_new(&x -> x_obj, &s_float);
+    x->out_gcd = outlet_new(&x->x_obj, &s_float);
 
-    x -> in_val_A = inlet_new(&x -> x_obj, &x -> x_obj.ob_pd, &s_float, gensym("inVal_A"));
-    x -> in_val_B = inlet_new(&x -> x_obj, &x -> x_obj.ob_pd, &s_float, gensym("inVal_B"));
+    x->in_val_A = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("inVal_A"));
+    x->in_val_B = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("inVal_B"));
 
     return (void *) x;
 }
 
 void gcd_free(t_gcd *x) {
-    outlet_free(x -> out_gcd);
+    outlet_free(x->out_gcd);
 }
 
 void gcd_setup(void) {
@@ -69,8 +78,9 @@ void gcd_setup(void) {
                           A_DEFFLOAT,
                           A_DEFFLOAT,
                           0);
-    class_addfloat(gcd_class, (t_method) gcd_onFloat);
     class_addbang(gcd_class, (t_method) gcd_onBang);
+    class_addlist(gcd_class, (t_method) gcd_onList);
+    class_addmethod(gcd_class, (t_method) gcd_onReset, gensym("reset"), 0);
     class_addmethod(gcd_class, (t_method) gcd_onSet_A, gensym("inVal_A"), A_DEFFLOAT, 0);
     class_addmethod(gcd_class, (t_method) gcd_onSet_B, gensym("inVal_B"), A_DEFFLOAT, 0);
 }
